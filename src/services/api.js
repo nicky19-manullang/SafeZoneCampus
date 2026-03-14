8d0e622a3ae3376db9f0e2b5c700facdc232a7dc
Nicky <nickysimanullang123@gmail.com>
2026-03-14 23:07:41 +0700

    s

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance - connect to backend API
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: import.meta.env.VITE_API_URL || '/api',
   headers: {
     'Content-Type': 'application/json'
   }
