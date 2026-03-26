d36dd5c2ecd41c781069a102260dfb387247f483
Nicky <nickysimanullang123@gmail.com>
2026-03-26 18:29:45 +0700

    delate backend

--- backend/add_lat_lng_columns.sql
+++
@@ -1,6 +0,0 @@
--- Add lat and lng columns to reports table
-ALTER TABLE reports ADD COLUMN lat DECIMAL(10, 8) NULL;
-ALTER TABLE reports ADD COLUMN lng DECIMAL(11, 8) NULL;
-
--- This script adds location coordinates support to existing databases
--- Run this to enable the map functionality
